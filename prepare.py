a6e3ecc19d94ccd1c28e3d174e25c2fd5f4a31ba
Mindy Shiben <mindy.shiben@gmail.com>
2022-05-20 12:53:14 -0500

    logistic_regression_practice

--- prepare.py
+++ prepare.py
@@ -16,6 +16,7 @@ def prep_iris(df):
 
 def prep_titanic(df):
     df = df.drop(['Unnamed: 0', 'passenger_id','pclass', 'deck', 'embarked'], axis=1)
+    df['embark_town'] = df.embark_town.dropna
     dummy_df = pd.get_dummies(df[['sex', 'embark_town', 'class']], dummy_na=False, drop_first=[True, True, True])
     df = pd.concat([dummy_df, df], axis=1)
     return df
@@ -24,7 +25,7 @@ def prep_telco(df):
     df = df.drop(['Unnamed: 0', 'customer_id'], axis=1)
     dummy = pd.get_dummies(df[['gender', 'partner', 'dependents', 'phone_service', 'multiple_lines',
                   'tech_support', 'streaming_tv', 'streaming_movies', 'paperless_billing',
-                  'churn', 'internet_service_type', 'payment_type', 'contract_type']],
+                  'churn', 'internet_service_type', 'payment_type', 'contract_type']], prefix= (['encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_', 'encoded_']),
               dummy_na=False, drop_first=[True, True, True, True, True, True, True, True, True, True, True, True, True])
     df = pd.concat([dummy, df], axis=1)
     return df
